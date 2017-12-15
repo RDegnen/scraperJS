93e21fcb7ab070edfaeb663625bc15cd689aabaf
RDegnen <rdegnen@gmail.com>
2017-12-15 15:25:41 -0500

    successfull write woooo

--- app/controllers/gatheringController.js
+++ app/controllers/gatheringController.js
@@ -3,6 +3,7 @@ const Gatherer = require('../models/gatherer');
 const create = (req, res, next) => {
   Gatherer.collectHtml(req)
     .then(results => Gatherer.writePageToDynamo(results, req.body.source))
+    .then(resp => res.json(resp))
     .catch(err => next(err));
 };
 
--- app/models/gatherer.js
+++ app/models/gatherer.js
@@ -7,13 +7,13 @@ const collectHtml = (req) => {
   return new Promise((resolve, reject) => {
     switch (req.body.source) {
       case 'craigslist': {
-        const rp1 = rp('https://boston.craigslist.org/d/internet-engineering/search/eng')
-        const rp2 = rp('https://boston.craigslist.org/d/software-qa-dba-etc/search/sof')
-        const rp3 = rp('https://boston.craigslist.org/d/web-html-info-design/search/web')
-
+        const rp1 = rp('https://boston.craigslist.org/d/internet-engineering/search/eng');
+        const rp2 = rp('https://boston.craigslist.org/d/software-qa-dba-etc/search/sof');
+        const rp3 = rp('https://boston.craigslist.org/d/web-html-info-design/search/web');
+        // Run all 3 request-promise promises and return array of html results
         Promise.all([rp1, rp2, rp3])
           .then(results => resolve(results))
-          .catch(err => reject(err))
+          .catch(err => reject(err));
         break;
       }
       default: {
@@ -25,21 +25,35 @@ const collectHtml = (req) => {
 
 const writePageToDynamo = (results, sourceData) => {
   return new Promise((resolve, reject) => {
-    const params = {
-      Item: {
-        html: {
-          S: results.html,
+    // Create array of PutRequest params to send to batchWriteItem
+    const paramsArray = [];
+    for (let i = 0; i < results.length; i++) {
+      const randomInt = Math.random() * (1 - 1000) + 1;
+      const params = {
+        PutRequest: {
+          Item: {
+            html: {
+              S: results[i],
+            },
+            source: {
+              S: sourceData,
+            },
+            pageId: {
+              S: `${sourceData}-${randomInt}`,
+            },
+          },
         },
-        source: {
-          S: sourceData,
-        },
-        pageId: {
-          S: results.pageId,
-        },
+      };
+      paramsArray.push(params);
+    }
+
+    const batchParams = {
+      RequestItems: {
+        scraped_pages: paramsArray,
       },
-      TableName: table,
     };
-    dynamodb.puteItem(params, (err, data) => {
+
+    dynamodb.batchWriteItem(batchParams, (err, data) => {
       if (err) reject(err);
       resolve('HTML successfully written to Dynamo');
     });
