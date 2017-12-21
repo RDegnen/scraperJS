ec6f282d3cee7a127553077f231a7e1a01da1fa2
Ross <rdegnen@gmail.com>
2017-12-20 20:19:39 -0500

    scrapped craigslist

--- app/models/scraper.js
+++ app/models/scraper.js
@@ -6,15 +6,21 @@ const scrapeCraigslist = (data) => {
   return new Promise((resolve, reject) => {
     const items = data.Items;
     const listings = [];
-    // for (let i = 0; i < items.length; i++) {
-    //   const $ = cheerio.load(items[i].html.S);
-    //   listings.push($('ul .rows'))
-    // }
-    const $ = cheerio.load(items[0].html.S);
-    // $('.result-row').each((i, elem) => {
-    //   resolve($(this));
-    // })
-    resolve($('.result-row').html());
+    for (let i = 0; i < items.length; i++) {
+      const $ = cheerio.load(items[0].html.S);
+      $('.result-row').each((j, elem) => {
+        const href = $(elem).find('a').attr('href');
+        const dataId = href.split('/');
+        const listing = {
+          link: href,
+          job_title: $(elem).find('.result-title').text(),
+          listing_name: `c${dataId[dataId.length - 1].split('.')[0]}`,
+          source: 'craigslist',
+        };
+        listings.push(listing);
+      });
+    }
+    resolve(listings);
   });
 };
 
