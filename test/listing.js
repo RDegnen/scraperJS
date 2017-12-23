5bddf6511fff5ad4e1f7394d56141acde847fb4f
RDegnen <rdegnen@gmail.com>
2017-12-22 20:08:36 -0500

    starting to promisify tests

--- test/listing.js
+++ test/listing.js
@@ -7,13 +7,28 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('/GET listings', () => {
-  it('should get all job listings', (done) => {
-    chai.request(app)
-      .get('/listings')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.should.be.a('object');
-        done();
-      });
+  it('should get all job listings', () => {
+    return new Promise((resolve) => {
+      chai.request(app)
+        .get('/listings')
+        .then((res) => {
+          res.should.have.status(200);
+          res.should.be.a('object');
+          resolve();
+        });
+    });
+  });
+});
+
+describe('/POST listings', () => {
+  it('should create all listings', () => {
+    return new Promise((resolve) => {
+      chai.request(app)
+        .post('listings/create/all')
+        .then((res) => {
+          res.should.have.status(200);
+          resolve();
+        });
+    });
   });
 });
