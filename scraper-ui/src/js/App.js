23a6c9d5733f1d4adf00d9a99f3d783cdae031dc
RDegnen <rdegnen@gmail.com>
2018-01-12 17:01:01 -0500

    figuring out this router

--- scraper-ui/src/js/App.js
+++ scraper-ui/src/js/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import Login from './components/login';
+
+// ReactDOM.render((
+//   <Router>
+//     <Route path='/login' Component={App}/>
+//   </Router>
+// ), document.getElementById('root'))
 
 class App extends Component {
   render() {
