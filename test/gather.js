607eba9588045913d175ba5049b6b9b7bf1cbff1
RDegnen <rdegnen@gmail.com>
2018-01-25 17:19:32 -0500

    figuring out integration testing for scraping

--- test/gather.js
+++ test/gather.js
@@ -3,10 +3,15 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
 const sinon = require('sinon');
+const sinonTestFactory = require('sinon-test');
 const Gatherer = require('../app/models/gatherer');
 const gatheringController = require('../app/controllers/gatheringController');
 
+const sinonTest = sinonTestFactory(sinon);
 const should = chai.should();
+const { expect } = chai;
+
+/* eslint func-names: ["error", "never"] */
 
 chai.use(chaiHttp);
 
@@ -25,16 +30,30 @@ describe('Gathering HTML', () => {
   };
 
   describe('Gathering all', () => {
-    it('should call gathering functions for all sites', () => {
-      const collectCraigslistHtml = sinon.stub(Gatherer, 'collectCraigslistHtml');
-      const collectIndeedHtml = sinon.stub(Gatherer, 'collectIndeedHtml');
+    it('should call gathering functions for all sites', sinonTest(function () {
+      const collectCraigslistHtmlSpy = this.stub(Gatherer, 'collectCraigslistHtml');
+      const collectIndeedHtmlSpy = this.stub(Gatherer, 'collectIndeedHtml');
       gatheringController.create(fakeReq, fakeRes, fakeNext);
 
-      collectCraigslistHtml.restore();
-      collectIndeedHtml.restore();
-
-      sinon.assert.calledWith(collectCraigslistHtml, fakeReq);
-      sinon.assert.calledWith(collectIndeedHtml, fakeReq);
-    });
+      sinon.assert.calledWith(collectCraigslistHtmlSpy, fakeReq);
+      sinon.assert.calledWith(collectIndeedHtmlSpy, fakeReq);
+    }));
+    // FIXME not sure how to test the calling of dynamo
+    it('should call writePageToDynamo with proper params', sinonTest(function () {
+      // const html = `<html>
+      //   <div>
+      //     <p> fake html </p>
+      //   </div>
+      // </html>`;
+      // const params = { location: 'new+york', source: 'indeed' };
+      const writeToDynamoStub = this.stub(Gatherer, 'writePageToDynamo');
+      const collectIndeedSpy = this.spy(Gatherer, 'collectIndeedHtml');
+      // writeToDynamoStub([html], params);
+      collectIndeedSpy(fakeReq)
+        .then((res) => {
+          expect(res).to.eql({ UnprocessedItems: {} });
+        })
+        .catch(err => console.log(err));
+    }));
   });
 });
