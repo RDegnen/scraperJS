cceb3d45a981d8005877caeaca234199e4671a42
RDegnen <rdegnen@gmail.com>
2018-02-02 15:42:06 -0500

    deleting listing removes from all filters now

--- scraper-ui/src/js/components/listings/jobListings.js
+++ scraper-ui/src/js/components/listings/jobListings.js
@@ -64,6 +64,8 @@ class JobListings extends Component {
     .then((res) => {
       console.log(res);
       this.state.jobListings.splice(params[2], 1)
+      this.state.currentInputFilter.splice(params[2], 1)
+      this.state.filteredListings.splice(params[2], 1)
       this.setState({ jobListings: this.state.jobListings  });
     })
     .catch(err => console.log(err));
